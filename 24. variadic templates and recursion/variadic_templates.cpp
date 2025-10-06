23eab483aa3de1e268f8af9b4c4bf5e31f44a9ff
Kumaravel Rajan <kumaravel.rajan7@gmail.com>
2025-10-06 11:04:48 +0200

    cpp

--- 24. variadic templates and recursion/variadic_templates.cpp
+++ 24. variadic templates and recursion/variadic_templates.cpp
@@ -16,6 +16,18 @@ void printFunc(T arg1, variadic_args_type... variadic_args)
     printFunc(variadic_args...);
 }
 
+template <typename T, typename... variadic_args_type>
+void print_Func_With_Fold_Expression(T arg1, variadic_args_type... variadic_args)
+{
+
+    cout << "#####################################################\nACCESSING VARIADIC_ARGS WITH FOLD EXPRESSION\n";
+    cout << "arg1 = " << arg1 << endl;
+
+    cout << "Now displaying arguments in variadic_args: " << endl;
+
+    ((cout << "Arg: " << variadic_args << endl), ...);
+}
+
 int main () {
 
     string myName = "Kumaravel";
@@ -47,7 +59,11 @@ int main () {
         → prints "printFunc 1: Kumaravel"
     */
 
-    printFunc(1, 2, 1.1, 2.2, myName);
+    cout << "#####################################################\nACCESSING VARIADIC_ARGS WITH RECURSION\n";
+
+    printFunc(1, 2, 1.1, 2.2, myName); /* Use recursion to unpack variadic_args */
+
+    print_Func_With_Fold_Expression(1, 2, 1.1, 2.2, myName); /* Uses fold expression to one shot print all variadic_args */
 
 	return 0;
 
