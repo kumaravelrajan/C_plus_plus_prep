d1760f54765d5cc345f0d93dbcace088f0d389aa
Kumaravel Rajan <kumaravel.rajan7@gmail.com>
2025-10-09 22:26:35 +0200

    cpp

--- Hitesh-CPP-bootcamp/33. smart pointer/weak_ptr.cpp
+++ Hitesh-CPP-bootcamp/33. smart pointer/weak_ptr.cpp
@@ -4,10 +4,12 @@
 
 using namespace std;
 
-struct B;
+class B; 
 
-struct A{
-    std::shared_ptr<B> ptrB; /* strong reference to B. This ptrB points to a dynamically created object of class B. The reference count of the memory block that contains the class B object goes up by 1. */
+class A{
+
+    public:
+    std::shared_ptr<B> ptrB;
 
     A(){
         cout << "Object A created\n";
@@ -18,8 +20,9 @@ struct A{
     }
 };
 
-struct B{
-    std::shared_ptr<A> ptrA; /* Strong reference to A. ptrA points to a dynamically created object of class A. The reference count of the mem block having this class A object goes up by 1. */
+class B{
+    public: 
+    std::weak_ptr<A> ptrA; /* MOST IMPORTANT PART */
 
     B(){
         cout << "Object B created.\n";
@@ -31,18 +34,22 @@ struct B{
 };
 
 int main(){
-    /* 
-    
-    Unique ptrs and shared ptrs when pointing to a mem block increase the reference count for the memory block they point to. They "own" the memory block. But specially in the case of shared pointers this might lead to the case of circular references where the reference count of two memory blocks does not get to 0 ever resulting in the shared pointers themselves going out of scope BUT the reference counts of the two mem blocks being pointed to never reaches 0. Hence, this results in memory leaks. Therefore, to solve this problem we have weak pointers.  
 
+    /* WEAK POINTERS SOLVING CYCLICAL REFERENCE ISSUE IN SHARED POINTERS -    
+    
+        If A and B both had shared_ptr for each other within them, this would increase heap_mem_block_A and heap_mem_block_B reference counts by 1 each. Remember, a mem block is deleted in heap ONLY IF its associated reference count is 0. But here in this code, even if ptr_to_objectA_in_heap and ptr_to_objectB_in_heap go out of scope at the end of main(), the heap_mem_block_A and heap_mem_block_B would still have reference count 1 (because they point to each other). This means these blocks will not be deleted and this causes memory leaks. 
+
+        On the other hand, A has shared_ptr to B and B has weak_ptr to A. This means when ptr_to_objectA_in_heap and ptr_to_objectB_in_heap go out of scope at end of main(), the ref counts of heap_mem_block_A would be 0 and ref count of heap_mem_block_B would be 1. As soon as heap_mem_block_A is deleted, ref count of heap_mem_block_B would also be 0. Hence, this is also deleted. Therefore, there is no memory leak.     
+    
     */
 
-    shared_ptr<A> objectA = make_shared<A>(); /* New class A object created in heap. */
-    shared_ptr<B> objectB = make_shared<B>(); /* New class B object created in heap. */
 
-    objectA->ptrB = objectB; 
+    shared_ptr<A> ptr_to_objectA_in_heap = make_shared<A>(); 
+    shared_ptr<B> ptr_to_objectB_in_heap = make_shared<B>(); 
 
-    objectB->ptrA = objectA;
+    ptr_to_objectA_in_heap->ptrB = ptr_to_objectB_in_heap;
+
+    ptr_to_objectB_in_heap->ptrA = ptr_to_objectA_in_heap;    
 
     return 0;
 }
