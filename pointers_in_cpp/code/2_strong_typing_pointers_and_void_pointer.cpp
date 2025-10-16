847bf419b3a752ec95b2a2cb9e779862bd6d7bac
Kumaravel Rajan <kumaravel.rajan7@gmail.com>
2025-10-16 23:36:09 +0200

    completed code and notes for 2.cpp

--- pointers_in_cpp/code/2_strong_typing_pointers_and_void_pointer.cpp
+++ pointers_in_cpp/code/2_strong_typing_pointers_and_void_pointer.cpp
@@ -7,38 +7,94 @@ using namespace std;
 
 int main(){
 
-	/* 1025 in binary = 
-	00000000 00000000 00000100 00000001 */
+	/* 1025 in binary = 00000000 00000000 00000100 00000001 */
 	int a = 1025;
 	
 	/* 32 bit int pointer */
 	int *p_int; 
 	p_int = &a;
 
-	cout << "Address of p_int = " << p_int << "; Value *p_int = " << *p_int << endl;
+	cout << "Address of p_int = " << p_int << "; Value *p_int = " << *p_int << endl; /* 0x7fffffffdaa4; 1025 */
 
-	cout << "Size of int = " << sizeof(int) << endl;
+	cout << "Size of int = " << sizeof(int) << endl; /* 4 */
 
-	cout << "Address of p_int + 1 = " << p_int + 1 << "; Value *(p_int + 1) = " << *(p_int + 1) << endl;
+	cout << "Address of p_int + 1 = " << p_int + 1 << "; Value *(p_int + 1) = " << *(p_int + 1) << endl; /* 0x7fffffffdaa8; -9564 (junk) */
 
-	/* 8 bit pointer */
-	int8_t *p_int8_t;
+	// #############################################################################################
+
+	/* char pointer */
+
+	// char* p_char = p_int;  --> Gives error. Thus, typecasting required. 
 	
-	// p_int8_t = p_int; --> Raises error. Hence, needs to be typecast. 
+	// #############################################################################################
 
-	p_int8_t = (int8_t*) p_int;
+	/* 
+		static_cast does not allow direct casting between pointers to unrelated types such as int* to char*. This conversion is ill-formed and forbidden by static_cast rules since int and char are unrelated fundamental types, and there is no inheritance or standard conversion that static_cast can apply here.
+		
+		The C++ standard restricts static_cast to conversions that maintain type safety, such as casting up and down within class hierarchies or converting to/from void*. Pointer conversions between unrelated types, like int* to char*, require reinterpret_cast, which performs a low-level cast without safety checks and directly converts the pointer bit pattern.
 
-	cout << "Address p_int8_t = " << static_cast<void*>(p_int8_t) << "; Value *p_int8_t = " << static_cast<int>(*p_int8_t) << endl;
+	*/
+	char* p_char = reinterpret_cast<char*>(p_int);
 
-	cout << "Size of p_int8_t = " << sizeof(int8_t) << endl;
+	// #############################################################################################
 
-	cout << "Address (p_int8_t + 1) = " << static_cast<void*>((p_int8_t + 1)) << "; Value *(p_int8_t + 1) = " << static_cast<int>(*(p_int8_t + 1)) << endl;
+	/*
+		The following does not work. 
+		
+		The reason you do not see the address in your cout statements with a char* pointer like p_char is due to how operator<< is overloaded for char* in C++. When you output a char*, cout treats it as a C-style string and prints the characters starting at the pointer's address until it encounters a null character ('\0'), instead of printing the pointer's actual memory address. 
 
+		p_char outputs the string starting at that address.
+
+		*p_char outputs the single character the pointer is pointing to.
+
+		p_char + 1 outputs the string starting from the next character.
+
+		*(p_char + 1) outputs the next single character.
+
+		If you want to print the actual pointer address instead of the string, you can cast the pointer to void*:
+	
+	*/
+
+	/* cout << "Address p_char = " << p_char << "; Value *p_char = " << *p_char;
+
+	cout << "Size of char = " << sizeof(char);
+
+	cout << "Address p_char + 1 = " << p_char + 1 << "; Value *(p_char + 1) = " << *(p_char + 1); */
+
+	// #############################################################################################
+
+	/* Casting the above code to void* to get pointer address and to int to get char value. */
+
+	cout << "Address p_char = " << static_cast<void*>(p_char) << "; Value *p_char = " << static_cast<int>(*p_char) << endl; /* 0x7fffffffdaa4; 1 */
+
+	cout << "Size of char = " << sizeof(char) << endl; /* 1 */
+
+	cout << "Address p_char + 1 = " << static_cast<void*>(p_char + 1) << "; Value *(p_char + 1) = " << static_cast<int>(*(p_char + 1)) << endl; /* 0x7fffffffdaa5; 4 */
+
+	/* 
+		CLINCHER:
+	
+		1025 in binary = 00000000 00000000 00000100 00000001
+
+		p_char initially points to least significant 8 bits. Hence we get the value 1. 
+
+		p_char+1 moves to the next 8 bits. Hence, we get the value 4. 
+
+	*/
+
+	// ######################################################################################################
+
+	/* void* type */
+	
+	void* p_void = p_int; /* Notice, no casting required here. */
+
+	cout << "Address p_void = " << p_void << endl; /* 0x7fffffffdaa4 */
+
+	// cout << "Value p_void = " << *p_void << endl; --> Not allowed.
+	
 	return 0;
 }
 
 /* TODO:
-
-	1. Explain why above static_cast to void* and int was required. (because int8_t seen by cout and it tries to print c-type string).
 	1. Add code for next part in video where void* pointer is shown. 
 */
--- pointers_in_cpp/readme.md
+++ pointers_in_cpp/readme.md
@@ -15,3 +15,41 @@ If p had been a char*, then the system would have only looked at one byte.
 Even if p had been a float* ```a```'s value would not have been retrieved correctly since the way floats and ints are stored are not the same. 
 
 This is explained [here](/pointers_in_cpp/code/2_strong_typing_pointers_and_void_pointer.cpp).
+
+In the file step by step the following is explained: 
+
+```cpp
+
+int a = 1025; /* 1025 in binary = 00000000 00000000 00000100 00000001 */
+int* p_int = &a;
+
+cout << p_int << *p_int; /* 0x7fffffffdaa4; 1025 */
+cout << p_int + 1 << *(p_int + 1); /* 0x7fffffffdaa8; -9564 (junk) */
+
+//#############################################
+
+char* p_char = (char*) p_int;
+
+cout << (void*) p_char << (int) *p_char; /* 0x7fffffffdaa4; 1 */
+
+cout << (void*) p_char << (int) *p_char; /* 0x7fffffffdaa5; 4 */
+
+```
+
+The reasoning for p_char displaying value as 1 and 4 respectively is that: 
+
+1. 1025 in binary = 00000000 00000000 00000100 00000001
+
+1. p_char initially points to least significant 8 bits. Hence we get the value 1. 
+
+1. p_char+1 moves to the next 8 bits. Hence, we get the value 4. 
+
+This is because seeing a character pointer the system only focusses on 8 bits even though the underlying value is of int (32 bits). 
+
+This clearly explains the need for strong typedness in pointers. 
+
+# void*
+
+The void pointer can receive addresses from any other fundamental type or any other fundamental type pointer. But C++ only allows void* to display addresses and not values. This is because void* do not correspond to a specific type. So the system does not know how many bits to look at when trying to find a value in the address being pointed to by void*.
+
+This is shown again [here](/pointers_in_cpp/code/2_strong_typing_pointers_and_void_pointer.cpp).
