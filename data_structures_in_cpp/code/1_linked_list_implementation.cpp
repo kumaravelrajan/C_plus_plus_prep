ad9decb23c49bc60dd21467bb4143d00f256945e
Kumaravel Rajan <kumaravel.rajan7@gmail.com>
2025-10-18 18:38:36 +0200

    linked list implementation

--- data_structures_in_cpp/code/1_linked_list_implementation.cpp
+++ data_structures_in_cpp/code/1_linked_list_implementation.cpp
@@ -11,32 +11,135 @@ struct Node{
 
     Node(int input_data){
         data = input_data;
+        next_node = nullptr;
+    }
+
+    ~Node(){
+        cout << "Node object destroyed\n";
     }
 };
 
-void print_linked_list(const Node* head_of_linked_list){
+struct LinkedList{
 
-    int position_in_linked_list = 0;
-    
-    for(const Node* temp = head_of_linked_list; temp != NULL; temp = temp->next_node.get()){
-        cout << "Address of node: " << temp << endl;
-        cout << "linked_list[" << position_in_linked_list << "].data = " << temp->data << endl;
-        cout << "linked_list[" << position_in_linked_list << "].next = " << temp->next_node.get() << endl << endl << endl;
+    std::unique_ptr<Node> head; 
 
-        position_in_linked_list++;
+    LinkedList(){
+        head = nullptr;
     }
 
-}
+    ~LinkedList(){
+        cout << "LinkedList object destroyed\n";
+    }
+
+    void add_element_to_beginning(int value_to_add){
+
+        unique_ptr<Node> temp = make_unique<Node>(value_to_add);
+
+        if(head){
+            temp->next_node = move(head);
+            head = move(temp);
+        } else {
+            head = move(temp);
+        }
+
+        print_linked_list();
+    }
+
+    void add_element_to_tail(int value_to_add){
+        
+        if(head){
+            Node* temp;
+
+            for(temp = head.get(); temp->next_node != nullptr; temp = temp->next_node.get());
+
+            temp->next_node = make_unique<Node>(value_to_add);            
+            
+        } else {
+            head = make_unique<Node>(value_to_add);
+        }
+
+        print_linked_list();
+    }
+
+    void remove_element_at_head(){     
+        
+        /* Try to remove element at head iff there is atleast 1 element in head. */
+        if(head){
+
+            Node* prev_temp; Node* temp;
+
+            temp = head.get();
+
+            /* Determine if there is only one element in the list or there are more elements. */
+
+            if(temp->next_node){
+
+                head = move(head->next_node);
+
+            } else {
+                /* There is only one element. Hence, simply deallocate head and move on.*/
+
+                head.reset();
+            }
+        }
+
+        print_linked_list();
+    }
+
+    void remove_element_at_tail(){
+        
+        /* Check list not empty. */
+        if(head){
+            /* Check if list has more than one element */
+
+            if(head->next_node){
+
+                /* List has more than one element. */
+
+                Node* temp = head.get();
+                Node *prev_temp;
+                
+                while(temp->next_node){
+                    prev_temp = temp;
+                    temp = temp->next_node.get();
+                }
+
+                prev_temp->next_node.reset();                
+
+            } else {
+                /* There is only one element. Hence, simply deallocate head and move on.*/
+
+                head.reset();
+            }
+        }
+
+        print_linked_list();
+    }
+
+    void print_linked_list(){
+
+        cout << "#####################################################################################\n";
+
+        for(const Node* temp = head.get(); temp != nullptr; temp = temp->next_node.get()){
+            cout << "Node address: " << temp << endl;
+            cout << "Data: " << temp->data << endl;
+            cout <<"Next: " << temp->next_node.get() << endl << endl << endl;
+        }
+    }
+
+};
 
 int main(){
 
-    Node linked_list_head(4);
+    LinkedList linked_list_obj;
 
-    linked_list_head.next_node = std::make_unique<Node>(5);;
+    linked_list_obj.add_element_to_beginning(1);
+    linked_list_obj.add_element_to_tail(2);
+    linked_list_obj.add_element_to_tail(3);
 
+    linked_list_obj.remove_element_at_head();
 
-
-    print_linked_list(&linked_list_head);
+    linked_list_obj.remove_element_at_tail();
 
 
 	return 0;
